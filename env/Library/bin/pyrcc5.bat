47b7de4fa56be089c3bf6e288cab8380a1acfc80
Greg Vargas <60016281+gvarg75@users.noreply.github.com>
2020-06-25 13:55:27 -0700

    remove env

--- env/Library/bin/pyrcc5.bat
+++
@@ -1,1 +0,0 @@
-@C:/Users/Gvargas/Documents/Springboard Data Science Bootcamp/Springboard/env\python.exe -m PyQt5.pyrcc_main %1 %2 %3 %4 %5 %6 %7 %8 %9
--- env/Library/bin/pyuic5.bat
+++
@@ -1,1 +0,0 @@
-@C:/Users/Gvargas/Documents/Springboard Data Science Bootcamp/Springboard/env\python.exe -m PyQt5.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
